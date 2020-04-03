869fc7a5bdc5d554b892578a6ad4a1aeeb5fd887
liuis <ping.li@conflux-chain.org>
2020-04-03 12:26:40 +0800

    fixed console bugs

--- packages/valve/console/commands_index.js
+++ packages/valve/console/commands_index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  compile: require("./compile4console"),
+  //compile: require("./compile4console"),
   //deploy: require("./deploy4console"),
   //sendbalance: require("./sendcfx4console"),
-  account: require("./account4console")
+  //account: require("./account4console")
 };
